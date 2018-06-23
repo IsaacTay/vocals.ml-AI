64b30d636444fa27c7e66c25d09b4cb5db512b55
Isaac Tay <IsaacTay@programmer.net>
2018-06-23 15:16:56 +0800

    Added SpeechEncodingModel

--- models.py
+++ models.py
@@ -6,15 +6,17 @@ import numpy as np
 
 class GenerativeModel(nn.Module):
     def __init__(self, input_size):
-        super(Model, self).__init__()
+        super(GenerativeModel, self).__init__()
         self.input_size = input_size
-        self.output_size = 256
+        self.input_channels = 512
+        self.output_channels = 256
         self.layers = nn.ModuleList([nn.Conv1d(self.output_size, self.output_size, 2, stride=2) for i in range(int(np.log2(self.input_size)))])
+        self.linear = nn.Linear(self.input_channels, self.output_channels)
         
     def forward(self, x, saves):
         if saves is None:
             saves = []
-            x = x.reshape(1, 256, -1)
+            x = x.reshape(1, self.input_channels, -1)
             for i, layer in enumerate(self.layers):
                 new_layer = nn.Conv1d(layer.in_channels, layer.out_channels, 2, dilation=pow(2, i))
                 new_layer.load_state_dict(layer.state_dict())
@@ -23,9 +25,37 @@ class GenerativeModel(nn.Module):
             saves = saves[:-1]
         else:
             new_saves = []
-            x = self.layers[0](x[-2:].reshape(1, 256, -1))
+            x = self.layers[0](x[-2:].reshape(1, self.input_channels, -1))
             for i, layer in enumerate(self.layers[1:]):
                 new_saves.append(saves[i-1][1:] + [x])
                 x = F.tanh(layer(torch.cat((saves[i-1][0], x), dim=2)))
-        x = x.reshape(1, 256)
+        x = x.reshape(self.input_channels)
+        x = self.linear(x)
+        x = x.reshape(1, self.output_channels)
+        return (x, saves)
+    
+class SpeechEncodingModel(nn.Module):
+    def __init__(self, input_size):
+        super(SpeechEncodingModel, self).__init__()
+        self.input_size = input_size
+        self.output_size = 256
+        self.layers = nn.ModuleList([nn.Conv1d(self.output_size, self.output_size, 3, stride=3) for i in range(int(np.log2(self.input_size)/np.log2(3)))])
+        
+    def forward(self, x, saves):
+        if saves is None:
+            saves = []
+            x = x.reshape(1, self.output_size, -1)
+            for i, layer in enumerate(self.layers):
+                new_layer = nn.Conv1d(layer.in_channels, layer.out_channels, 3, dilation=pow(3, i))
+                new_layer.load_state_dict(layer.state_dict())
+                x = F.tanh(new_layer(x))
+                saves.append(list(torch.chunk(x, x.shape[-1], dim=2)[-pow(3, i+1):]))
+            saves = saves[:-1]
+        else:
+            new_saves = []
+            x = self.layers[0](x[-:].reshape(1, self.output_size, -1))
+            for i, layer in enumerate(self.layers[1:]):
+                new_saves.append(saves[i-1][1:] + [x])
+                x = F.tanh(layer(torch.cat((saves[i-1][0], x), dim=2)))
+        x = x.reshape(1, self.output_size)
         return (x, saves)
