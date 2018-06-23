85beb4640f40b4eea65b9b939a6def7aeddd25f9
Isaac Tay <IsaacTay@programmer.net>
2018-06-23 16:46:14 +0800

    Create psuedo-train.py

---
+++ psuedo-train.py
@@ -0,0 +1,14 @@
+import utils
+
+voice_audio_files = ["The_voice.wav"]
+voice_audio = [utils.toULaw(utils.importAudio(voice_audio_file)) for voice_audio_file in voice_audio_files]
+
+general_audio_files = ["Other voices".wav]
+general_audio = [utils.toULaw(utils.importAudio(general_audio_file)) for general_audio_file in general_audio_files]
+
+loop:
+    best_dis = best(dises)
+    train_dis
+    train_gen+enc
+  
+10/10 donez
