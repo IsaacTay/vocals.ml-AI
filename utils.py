df60ce63b754110005c4c5fb4fced5d6d2edb874
Isaac Tay <IsaacTay@programmer.net>
2018-06-23 14:09:48 +0800

    Added uLaw encoding/deconding
    Finished #3

--- utils.py
+++ utils.py
@@ -3,3 +3,12 @@ import librosa
 def importAudio(path):
   audio, _ = librosa.load(path, sr=16000)
   return audio
+
+def toULaw(audio):
+  audio = np.floor(np.sign(audio)*(np.log(1+255*np.abs(audio))/np.log(256)) * 127.5 + 128)
+  return audio
+
+def toLin(output):
+  output = output/127.5 - 1
+  audio = np.sign(output) * (np.power(256,output) - 1) / 255
+  return audio
